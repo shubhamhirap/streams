1094b9e8635da0ae769ffadcba8d6837db0e4308
shubham hirap <shirap07@gmail.com>
2020-12-17 13:53:17 +0530

    google auth with react

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+export class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "988657563839-u5l1e41688s8811qg81f99iclur2fej4.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button
+          className="ui red google button"
+          onClick={() => this.onSignOutClick()}
+        >
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button
+          className="ui red google button"
+          onClick={() => this.onSignInClick()}
+        >
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import GoogleAuth from "./GoogleAuth";
 
 const Header = () => {
-    return (
-        <div className="ui secondary pointing menu">
-            <Link to="/" className="item">Streamy</Link>
-            <div className="right menu">
-                <Link to="/" className="item">All stream</Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All stream
+        </Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
