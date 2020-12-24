fbf859a81d333b37f9c890e4002f929e3127362f
shubham hirap <shirap07@gmail.com>
2020-12-24 16:26:35 +0530

    delete stream modal using react portals - work in progress

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
-    return (
-        <div>
-            Stream Delete
-        </div>
-    )
-}
+  const actions = (
+    <>
+      <button className="ui button">Cancel</button>
+      <button className="ui negative button">Delete</button>
+    </>
+  );
+  return (
+    <div>
+      Stream Delete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
+    </div>
+  );
+};
 
-export default StreamDelete
+export default StreamDelete;
