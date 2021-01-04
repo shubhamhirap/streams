7ada8ae9c028e3614167e688cd4bc61abd2a52b8
shubham hirap <shirap07@gmail.com>
2021-01-04 19:06:12 +0530

    rtmp server setup and live stream from local machine

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -12,6 +12,31 @@ class StreamShow extends Component {
 
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
